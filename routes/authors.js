9d7c43995edb7c25b1d37f36396d133ce9bb2c83
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-14 14:53:41 +0000

    /authors endpoint - basic requests added, no validation yet

--- routes/authors.js
+++ routes/authors.js
@@ -4,4 +4,30 @@ const Author = require("../models/Author");
 
 const authorRouter = Router();
 
+authorRouter.get("/", async (req, res) => {
+  const allAuthors = await Author.findAll();
+  res.status(302).send(allAuthors);
+});
+
+authorRouter.get("/:id", async (req, res) => {
+  const singleAuthor = await Author.findByPk(req.params.id);
+  res.status(302).send(singleAuthor);
+});
+
+authorRouter.post("/", async (req, res) => {
+  const newAuthor = await Author.create(req.body);
+  res.status(201).send(newAuthor);
+});
+
+authorRouter.delete("/:id", async (req, res) => {
+  const authorToDelete = await Author.findByPk(req.params.id);
+  const authorName = authorToDelete.name;
+  await Author.destroy({
+    where: {
+      name: authorName,
+    },
+  });
+  res.status(204).send(`${authorName} successfully deleted`);
+});
+
 module.exports = authorRouter;
