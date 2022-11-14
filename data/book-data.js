40d716e749db2717544f04ef1dced18cb83568c7
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-14 10:10:56 +0000

    book data added

---
+++ data/book-data.js
@@ -0,0 +1,90 @@
+const books = [
+  {
+    title: "the girls who disappeared",
+    image: "https://m.media-amazon.com/images/I/51pD-JvYH0L._SY346_.jpg",
+    genre: "mystery",
+    price: 9.99,
+    rating: 4.3,
+    reviews: 0,
+  },
+  {
+    title: "the bullet that missed",
+    image:
+      "https://m.media-amazon.com/images/I/51mqUQxstoL._SX323_BO1,204,203,200_.jpg",
+    genre: "mystery",
+    price: 10.0,
+    rating: 4.7,
+    reviews: 0,
+  },
+  {
+    title: "22 seconds",
+    image: "https://m.media-amazon.com/images/I/51jyXb9vahL._SY346_.jpg",
+    genre: "mystery",
+    price: 9.99,
+    rating: 4.5,
+    reviews: 0,
+  },
+  {
+    title: "verity",
+    image:
+      "https://m.media-amazon.com/images/I/51sfWeyiskL._SX316_BO1,204,203,200_.jpg",
+    genre: "thriller",
+    price: 4.5,
+    rating: 4.5,
+    reviews: 0,
+  },
+
+  {
+    title: "desert star",
+    image: "https://m.media-amazon.com/images/I/51StbfuRowL._SY346_.jpg",
+    genre: "thriller",
+    price: 11.99,
+    rating: 4.7,
+    reviews: 0,
+  },
+  {
+    title: "fire and blood - A song of ice and fire",
+    image:
+      "https://cdn.waterstones.com/bookjackets/large/9780/0084/9780008402785.jpg",
+    genre: "fantasy",
+    price: 7.99,
+    rating: 3.5,
+    reviews: 0,
+  },
+  {
+    title: "three sisters",
+    image:
+      "https://cdn.waterstones.com/bookjackets/large/9781/8387/9781838774592.jpg",
+    genre: "historical fiction",
+    price: 7.49,
+    rating: 4.5,
+    reviews: 0,
+  },
+  {
+    title: "pride and prejudice",
+    image:
+      "https://cdn.waterstones.com/override/v1/large/9780/1410/9780141040349.jpg",
+    genre: "classic",
+    price: 14.99,
+    rating: 4.8,
+    reviews: 0,
+  },
+  {
+    title: "little women",
+    image:
+      "https://m.media-amazon.com/images/P/0147514010.01._SCLZZZZZZZ_SX500_.jpg",
+    genre: "classic",
+    price: 14.99,
+    rating: 4.8,
+    reviews: 0,
+  },
+  {
+    title: "lioness: my journey to glory",
+    image:
+      "https://cdn.waterstones.com/bookjackets/large/9781/3996/9781399611671.jpg",
+    genre: "autobiography",
+    price: 16.99,
+    rating: 5.0,
+    reviews: 0,
+  },
+];
