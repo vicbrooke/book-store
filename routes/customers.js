d8664df7cd1f5cdadc22792ecb9cf4827ca4c269
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-14 15:51:00 +0000

    /customers endpoint - basic requests added, no validation yet

--- routes/customers.js
+++ routes/customers.js
@@ -4,4 +4,31 @@ const Customer = require("../models/Customer");
 
 const customerRouter = Router();
 
+customerRouter.get("/", async (req, res) => {
+  const allCustomers = await Customer.findAll();
+  res.status(302).send(allCustomers);
+});
+
+customerRouter.get("/:id", async (req, res) => {
+  const singleCustomer = await Customer.findByPk(req.params.id);
+  res.status(302).send(singleCustomer);
+});
+
+customerRouter.post("/", async (req, res) => {
+  const newCustomer = await Customer.create(req.body);
+  res.status(201).send(newCustomer);
+});
+
+customerRouter.put("/:id", async (req, res) => {
+  await Customer.update(req.body, { where: { id: req.params.id } });
+  const updatedCustomer = await Customer.findByPk(req.params.id);
+  res.status(202).send(updatedCustomer);
+});
+
+customerRouter.delete("/:id", async (req, res) => {
+  const { name } = await Customer.findByPk(req.params.id);
+  await Customer.destroy({ where: { id: req.params.id } });
+  res.status(202).send(`'${name}' was successfully deleted`);
+});
+
 module.exports = customerRouter;
